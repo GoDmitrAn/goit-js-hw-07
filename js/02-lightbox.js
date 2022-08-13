dd1f5ad93f7e0b7d360d29d088dd0149b32dec23
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-08-13 23:34:18 +0300

    fix homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,13 +3,14 @@ import { galleryItems } from './gallery-items.js';
 const mainGalleryTask2 = document.querySelector('.gallery');
 function createItemTemplateTask2({preview, original,description}) {
     
-    const template = `<a class="gallery__item" href="${original}">
+    const template = `<li><a class="gallery__item" href="${original}">
                             <img class="gallery__image"
                                 src="${preview}" 
                                 alt="${description}" 
-                                title="${description}"
+                                
                             />
-                            </a>` ;
+                            </a>
+                        </li>` ;
     return template;
 }
 function renderGalleryItemsTask2() {
@@ -17,5 +18,8 @@ function renderGalleryItemsTask2() {
     mainGalleryTask2.insertAdjacentHTML('beforeend', fullTemplate);
 }
 renderGalleryItemsTask2();
-let lightbox = new SimpleLightbox('.gallery a',{captionDelay:250});
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt" , 
+    captionDelay: 250,
+});
 console.log(galleryItems);
